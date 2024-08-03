002a23733d3fa4bbfb250cfe10d6b2db90fb10d3
yokoba <31529581+yokoba@users.noreply.github.com>
2024-08-03 22:21:50 +0900

    #4 Moving objects animation + Collision detection

--- src/script.js
+++ src/script.js
@@ -10,13 +10,19 @@ canvas.height = window_height;
 
 canvas.style.background = '#ff8';
 
+let hit_counter = 0;
+
 class Circle {
-    constructor(xpos, ypos, radius, color, text) {
+    constructor(xpos, ypos, radius, color, text, speed) {
         this.xpos = xpos;
         this.ypos = ypos;
         this.radius = radius;
         this.color = color;
         this.text = text;
+        this.speed = speed;
+
+        this.dx = 1 * this.speed;
+        this.dy = 1 * this.speed;
     }
 
     draw(context) {
@@ -30,26 +36,67 @@ class Circle {
         // context.strokeText(this.text, this.xpos, this.ypos);
 
         context.lineWidth = 5;
-        context.arc(this.xpos, this.ypos, this.radius, 0, Math.PI * 2, false);
+        context.arc(
+            this.xpos,
+            this.ypos,
+            this.radius,
+            0,
+            Math.PI * 2,
+            false,
+            1,
+        );
         context.stroke();
         context.closePath();
     }
+
+    update() {
+        this.text = hit_counter;
+
+        context.clearRect(0, 0, window_width, window_height);
+        this.draw(context);
+
+        if (this.xpos + this.radius > window_width) {
+            this.dx = -this.dx;
+            hit_counter++;
+        }
+
+        if (this.xpos - this.radius < 0) {
+            this.dx = -this.dx;
+            hit_counter++;
+        }
+
+        if (this.ypos - this.radius < 0) {
+            this.dy = -this.dy;
+            hit_counter++;
+        }
+
+        if (this.ypos + this.radius > window_height) {
+            this.dy = -this.dy;
+            hit_counter++;
+        }
+
+        this.xpos += this.dx;
+        this.ypos += this.dy;
+    }
 }
 
-let circle_counter = 1;
+let my_circle = new Circle(100, 100, 50, 'black', hit_counter, 10);
+my_circle.draw(context);
 
-let all_circles = [];
-let createCircle = function (circle) {
-    circle.draw(context);
+let updateCircle = function () {
+    requestAnimationFrame(updateCircle);
+    my_circle.update();
 };
 
-for (var number = 0; number < 10; number++) {
-    let random_x = Math.random() * window_width;
-    let random_y = Math.random() * window_height;
-    let my_circle = new Circle(random_x, random_y, 50, 'black', circle_counter);
-    all_circles.push(my_circle);
-    createCircle(all_circles[number]);
-    circle_counter++;
-}
+updateCircle();
 
-console.log(all_circles);
+// for (var number = 0; number < 1; number++) {
+//     let random_x = Math.random() * window_width;
+//     let random_y = Math.random() * window_height;
+//     let my_circle = new Circle(random_x, random_y, 50, 'black', circle_counter);
+//     all_circles.push(my_circle);
+//     createCircle(all_circles[number]);
+//     circle_counter++;
+// }
+
+// console.log(all_circles);
