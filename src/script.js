237f0637ef480f7e2de2642a3c32e85ba8625ba4
yokoba <31529581+yokoba@users.noreply.github.com>
2024-08-03 22:21:40 +0900

    #2 Creating classes and object circles dynamic

--- src/script.js
+++ src/script.js
@@ -10,14 +10,34 @@ canvas.height = window_height;
 
 canvas.style.background = '#ff8';
 
-context.fillRect(300, 0, 100, 200);
+class Circle {
+    constructor(xpos, ypos, radius, color) {
+        this.xpos = xpos;
+        this.ypos = ypos;
+        this.radius = radius;
+        this.color = color;
+    }
 
-context.fillStyle = 'red';
-context.fillRect(100, 500, 100, 100);
+    draw(context) {
+        context.beginPath();
+        context.lineWidth = 5;
+        context.arc(this.xpos, this.ypos, this.radius, 0, Math.PI * 2, false);
+        context.stroke();
+        context.closePath();
+    }
+}
 
-context.beginPath();
-context.strokeStyle = 'blue';
-context.lineWidth = 20;
-context.arc(100, 100, 50, 0, Math.PI * 2, false);
-context.stroke();
-context.closePath();
+let all_circles = [];
+let createCircle = function (circle) {
+    circle.draw(context);
+};
+
+for (var number = 0; number < 10; number++) {
+    let random_x = Math.random() * window_width;
+    let random_y = Math.random() * window_height;
+    let my_circle = new Circle(random_x, random_y, 50, 'black');
+    all_circles.push(my_circle);
+    createCircle(all_circles[number]);
+}
+
+console.log(all_circles);
