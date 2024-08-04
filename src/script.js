d7b0d442b3439ccbf4afb0c51f5c969c11924e41
yokoba <31529581+yokoba@users.noreply.github.com>
2024-08-04 17:16:02 +0900

    #8 HTML5 Canvas JavaScript Tutorial

--- src/script.js
+++ src/script.js
@@ -8,32 +8,60 @@ var window_width = window.innerWidth;
 canvas.width = window_width;
 canvas.height = window_height;
 
-canvas.style.background = '#ff8';
+canvas.style.background = '#bbf';
 
-class Image {
-    constructor(imagePath, xpos, ypos, width, height) {
-        this.imagePath = imagePath;
-        this.xpos = xpos;
-        this.ypos = ypos;
-        this.width = width;
-        this.height = height;
+class Circle {
+    constructor(xpoint, ypoint, radius, color) {
+        this.xpoint = xpoint;
+        this.ypoint = ypoint;
+        this.radius = radius;
+        this.color = color;
+    }
+
+    draw(context) {
+        context.beginPath();
+        context.arc(
+            this.xpoint,
+            this.ypoint,
+            this.radius,
+            0,
+            Math.PI * 2,
+            false
+        );
+        context.strokeStyle = 'gray';
+        context.lineWidth = 3;
+        context.fillStyle = this.color;
+        context.fill();
+        context.stroke();
+        context.closePath();
+    }
+
+    changeColor(newColor) {
+        this.color = newColor;
+        this.draw(context);
+    }
+
+    clickCircle(xmouse, ymouse) {
+        const distance = Math.sqrt(
+            Math.pow(xmouse - this.xpoint, 2) +
+                Math.pow(ymouse - this.ypoint, 2)
+        );
+        if (distance < this.radius) {
+            this.changeColor('#56f');
+            return true;
+        } else {
+            this.changeColor('#f56');
+            return false;
+        }
     }
 }
 
-function createImage(context, imagePath, xpos, ypos, width, height) {
-    let myImage = document.createElement('img');
-    myImage.src = imagePath;
-    myImage.onload = function () {
-        context.drawImage(myImage, xpos, ypos, width, height);
-    };
-}
+let circle = new Circle(200, 200, 100, '#f56');
+circle.draw(context);
 
-let image = new Image('./DALL-E-01.png', 50, 50, 100, 100);
-createImage(
-    context,
-    image.imagePath,
-    image.xpos,
-    image.ypos,
-    image.width,
-    image.height
-);
+canvas.addEventListener('click', (event) => {
+    const rect = canvas.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+    circle.clickCircle(x, y);
+});
